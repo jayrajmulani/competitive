5fe4db885624fd668db739bd3cb506fd94d9eb0f
Jayraj Mulani <jayrajm.mulani@gmail.com>
2021-06-06 11:01:12 +0530

    Palindrome

---
+++ PalindromeGame_B1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class PalindromeGame_B1{
+    public static void main(String args[]){
+        Scanner sc  = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int t=0;t<T;t++){
+            int n = sc.nextInt();
+            String s = sc.next();
+            System.out.println("BOB");
+        }
+}
